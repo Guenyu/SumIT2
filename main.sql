68e2935106894a33990500bc2fe465f0281f92eb
tae1 <ttenill@naver.com>
2016-04-01 15:25:20 +0900

    DB,DAO변경

--- main.sql
+++ main.sql
@@ -2,11 +2,10 @@ create table UserInFo(
 	email varchar2(30) primary key,
 	password varchar2(30) not null,
 	name varchar2(15) not null,
-	age varchar2(30) not null,
-	id varchar2(30) not null,
-	addr varchar2(50) not null,
+--	age varchar2(30) not null,
+--	addr varchar2(50) not null,
 	phone varchar2(20) not null,
-	gender char(1) not null
+--	gender char(1) not null
 );
 select * from UserInFo;
 drop table UserInFo;
