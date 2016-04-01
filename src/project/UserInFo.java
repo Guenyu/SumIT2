d7e6d9a7078f379c122284dd0ba8040bdd22e9d2
tae1 <ttenill@naver.com>
2016-04-01 15:47:56 +0900

    DB,DAO 변경

--- src/project/UserInFo.java
+++ src/project/UserInFo.java
@@ -4,11 +4,11 @@ public class UserInFo {
 	private String email;
 	private String password;
 	private String name;
-	private String age;
-	private String id;
-	private String addr;
+//	private String age;
+//	private String id;
+//	private String addr;
 	private String phone;
-	private String gender;
+//	private String gender;
 	public String getEmail() {
 		return email;
 	}
@@ -27,35 +27,35 @@ public class UserInFo {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getAge() {
-		return age;
-	}
-	public void setAge(String age) {
-		this.age = age;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getAddr() {
-		return addr;
-	}
-	public void setAddr(String addr) {
-		this.addr = addr;
-	}
+//	public String getAge() {
+//		return age;
+//	}
+//	public void setAge(String age) {
+//		this.age = age;
+//	}
+//	public String getId() {
+//		return id;
+//	}
+//	public void setId(String id) {
+//		this.id = id;
+//	}
+//	public String getAddr() {
+//		return addr;
+//	}
+//	public void setAddr(String addr) {
+//		this.addr = addr;
+//	}
 	public String getPhone() {
 		return phone;
 	}
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
+//	public String getGender() {
+//		return gender;
+//	}
+//	public void setGender(String gender) {
+//		this.gender = gender;
+//	}
 	
 }
